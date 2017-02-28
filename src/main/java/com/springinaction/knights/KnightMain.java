db1243b1601542e1c4f3c641abd34d765dca0bf4
guangtoutou <ni.ningning@gmail.com>
2017-02-28 17:24:15 +0800

    add KnightTest
    use JUnit 4.11

--- src/main/java/com/springinaction/knights/KnightMain.java
+++ src/main/java/com/springinaction/knights/KnightMain.java
@@ -3,13 +3,11 @@ package com.springinaction.knights;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = KnightConfigAuto.class)
+
 public class KnightMain {
-	@Autowired
-	private static Knight knight;
+
 
 	public static void main(String[] args) {
-		knight.embarkOnQuest();
 
 	}
 
