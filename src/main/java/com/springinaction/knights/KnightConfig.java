36f67e90d90d4a66e439aeb648ee1f4ce49dd3db
guangtoutou <ni.ningning@gmail.com>
2017-03-04 22:29:24 +0800

    finetune

--- src/main/java/com/springinaction/knights/KnightConfig.java
+++ src/main/java/com/springinaction/knights/KnightConfig.java
@@ -1,19 +1,18 @@
 package com.springinaction.knights;
 
+import java.io.PrintStream;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class KnightConfig {
 
-/*	@Bean
-	public Knight setKnight(){
-		return new BraveKnight(quest());
-	}
+@Bean
+public PrintStream setStream(){
+	return System.out;
+}
 	
-	@Bean
-	public Quest quest(){
-		return new SlayDragonQuest(System.out);
-	}
-	*/
 }
